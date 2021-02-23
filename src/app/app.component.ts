ce17d5741e2d00a982f9b9a7ad4a2514c55fc9ab
MichaelGitHub77 <ms970722@gmail.com>
2021-02-23 19:29:32 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component, VERSION } from '@angular/core';
+import { FunctionCall } from "@angular/compiler";
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  public items: any[][];
+  x = null;
+  y = null;
+  a = 0;
+  constructor() {
+    this.items = [
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"]
+    ];
+  }
+  Input() {
+    this.x = this.x * 1;
+    this.y = this.y * 1;
+    var win1 = 0;
+    var win2 = 0;
+
+    for (var i = 0; i < 5; i++) {
+      win1 = 0;
+      win2 = 0;
+      for (var j = 0; j < 5; j++) {
+        if (this.items[i][j] == 1) {
+          win1++;
+        }
+        if (this.items[i][j] == 2) {
+          win2++;
+        }
+        if (win1 == 4) {
+          alert("player 1 win");
+          this.Reset();
+          break;
+        }
+        if (win2 == 4) {
+          alert("player 2 win");
+          this.Reset();
+          break;
+        }
+      }
+    }
+
+    for (var j = 0; j < 5; j++) {
+      win1 = 0;
+      win2 = 0;
+      for (var i = 0; i < 5; i++) {
+        if (this.items[i][j] == 1) {
+          win1++;
+        }
+        if (this.items[i][j] == 2) {
+          win2++;
+        }
+        if (win1 == 4) {
+          alert("player 1 win");
+          this.Reset();
+          break;
+        }
+        if (win2 == 4) {
+          alert("player 2 win");
+          this.Reset();
+          break;
+        }
+      }
+    }
+
+    if (this.x < 5 && this.y < 5) {
+      if (this.a % 2 == 0) {
+        if (
+          this.items[this.x][this.y] != 1 &&
+          this.items[this.x][this.y] != 2
+        ) {
+          this.items[this.x][this.y] = "1";
+        } else {
+          alert("error");
+        }
+      }
+      if (this.a % 2 == 1) {
+        if (
+          this.items[this.x][this.y] != 1 &&
+          this.items[this.x][this.y] != 2
+        ) {
+          this.items[this.x][this.y] = "2";
+        } else {
+          alert("error");
+        }
+      }
+    } else {
+      alert("error");
+    }
+    this.a++;
+  }
+  Reset() {
+    this.items = [
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"],
+      ["*", "*", "*", "*", "*"]
+    ];
+  }
 }
